c73af3de608910e1f8cac8dd1bc9616273336c2f
opalthailand <opalthailand@gmail.com>
2024-12-04 17:20:04 +0700

    fff

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
 from setuptools import setup, find_packages
 import sys, os
+
 version = '0.1'
+
 setup(
-	name='ckanext-linenotify',
+	name='ckanext-notification',
 	version=version,
-	description="LINE Notify plugin",
+	description="Twitter notification plugin",
 	long_description="""\
 	""",
-	classifiers=[],
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	keywords='',
-	author='Your Name',  # Update this
-	author_email='your.email@example.com',  # Update this
+	author='Takahiro Shida',
+	author_email='shida@intellilink.co.jp',
 	url='',
 	license='',
 	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.linenotify'],
+	namespace_packages=['ckanext', 'ckanext.notification'],
 	include_package_data=True,
 	zip_safe=False,
 	install_requires=[
-        'requests'
+		# -*- Extra requirements: -*-
 	],
 	entry_points=\
 	"""
         [ckan.plugins]
-	linenotify=ckanext.linenotify.plugin:LINENotifyPlugin
+	# Add plugins here, eg
+	notification=ckanext.notification.plugin:TwitterNotificationPlugin
 	""",
 )
