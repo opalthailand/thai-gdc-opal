a9cf0d72c0e2a1cee85bbba7ce3a0ac6ce0aa139
opalthailand <opalthailand@gmail.com>
2024-12-09 15:54:56 +0700

    fff

--- ckanext/orgextra/plugin.py
+++ ckanext/orgextra/plugin.py
@@ -2,13 +2,24 @@
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import requests
 
+def send_line_notification(message):
+    url = "https://notify-api.line.me/api/notify"
+    headers = {"Authorization":"Bearer cw37fBYJd9VAS45mLDXEtKmSpdpduuEyRO2BFVN2TrW"}
+    data = {"message": message}
+    response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
 
 class OrgextraPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IAuthenticator)
 
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
+
+    def login(self):
+        send_line_notification("TTEESSTT")
