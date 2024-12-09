d16671a7bb567136c88e2ab9b38c0c229c07ad39
opalthailand <opalthailand@gmail.com>
2024-12-09 12:46:09 +0700

    ff

--- ckanext/orgextra/plugin.py
+++ ckanext/orgextra/plugin.py
@@ -2,6 +2,8 @@
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.model as model
+
 
 
 class OrgextraPlugin(plugins.SingletonPlugin):
@@ -9,9 +11,17 @@ class OrgextraPlugin(plugins.SingletonPlugin):
 
     # IConfigurer
 
+    def get_users(self):
+        try:
+            users = model.User.all()
+            return [user.name for user in users]
+        except Exception as e:
+            return []  # Handle potential errors gracefully
+
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_public_directory(config_, 'fanstatic')
         toolkit.add_resource('fanstatic',
             'orgextra')
+        toolkit.add_template_global(config_, 'get_users', self.get_users)
