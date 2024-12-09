de97e6f1f31e9faac25c50fab1a86a60874a4b2d
opalthailand <opalthailand@gmail.com>
2024-12-09 16:05:02 +0700

    f

--- ckanext/orgextra/plugin.py
+++ ckanext/orgextra/plugin.py
@@ -7,15 +7,16 @@ import requests
 LINE_NOTIFY_TOKEN = "cw37fBYJd9VAS45mLDXEtKmSpdpduuEyRO2BFVN2TrW" # Replace with your token
 
 def send_line_notification(message):
-    url = "https://notify-api.line.me/api/notify"
-    headers = {"Authorization": "Bearer " + LINE_NOTIFY_TOKEN}
-    data = {"message": message}
-    try:
-        response = requests.post(url, headers=headers, data=data)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        # Print the error for now.  Ideally, use a logger here.
-        print("Error sending Line notification",e)
+    # url = "https://notify-api.line.me/api/notify"
+    # headers = {"Authorization": "Bearer " + LINE_NOTIFY_TOKEN}
+    # data = {"message": message}
+    # try:
+    #     response = requests.post(url, headers=headers, data=data)
+    #     response.raise_for_status()
+    # except requests.exceptions.RequestException as e:
+    #     # Print the error for now.  Ideally, use a logger here.
+    #     print("Error sending Line notification",e)
+    print('yes')
 
 
 class OrgextraPlugin(plugins.SingletonPlugin):
@@ -28,20 +29,7 @@ class OrgextraPlugin(plugins.SingletonPlugin):
 
     def login(self, context, data, user):
         if user: # check if a user object exists before trying to use it
-            send_line_notification("User {0} logged in.".format(user.get('name') or user.get('id')))
+            send_line_notification("User")
         else:
             print("Login attempt with no user information.") #  Handle this case as needed.
         return (context, data, user)
-
-    # Other IAuthenticator methods (these are often required.  Add implementations or pass if not used)
-    def identify(self, request):
-        pass 
-
-    def authenticate(self, context, data):
-        pass
-
-    def logout(self, context, user):
-        pass
-
-    def abort(self, context, data, user):
-        pass
